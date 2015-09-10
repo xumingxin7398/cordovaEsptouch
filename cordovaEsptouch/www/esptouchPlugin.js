e9ebc05629aeced6069a8232c3a8d1ac70974049
xumingxin <408676083@qq.com>
2015-09-10 14:23:02 +0800

    recommit

--- cordovaEsptouch/www/esptouchPlugin.js
+++
@@ -1,8 +0,0 @@
-var exec    = require('cordova/exec'),
-cordova = require('cordova');
-
-module.exports = {
-	smartConfig:function(apSsid,apBssid,apPassword,isSsidHiddenStr,taskResultCountStr,successCallback, errorCallback){
-		exec(successCallback, errorCallback, "esptouchPlugin", "smartConfig", [apSsid,apBssid,apPassword,isSsidHiddenStr,taskResultCountStr]);
-	}
-}
