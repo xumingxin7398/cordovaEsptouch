e30d048489cc976812eafcf256404550bf0bd6eb
xumingxin <408676083@qq.com>
2015-09-10 17:53:26 +0800

    recommit

--- src/android/esptouchPlugin.java
+++ src/android/esptouchPlugin.java
@@ -29,66 +29,78 @@ public class esptouchPlugin extends CordovaPlugin {
     public boolean execute(String action, final JSONArray args,final CallbackContext callbackContext) throws JSONException{
         if (action.equals("smartConfig")) {
 			receivingCallbackContext = callbackContext;
-            callbackContext.success("enter smartConfig");
             final String apSsid = args.getString(0);
 			final String apBssid = args.getString(1);
 			final String apPassword = args.getString(2);
 			final String isSsidHiddenStr = args.getString(3);
 			final String taskResultCountStr = args.getString(4);
-			
+			final int taskResultCount = Integer.parseInt(taskResultCountStr);
 			final Object mLock = new Object();
 			cordova.getThreadPool().execute(
 				new Runnable() {
 					public void run() {
-						receivingCallbackContext.success("thread runing");
 						synchronized (mLock) {
-							PluginResult result = new PluginResult(PluginResult.Status.OK, "thread runing");
-                            result.setKeepCallback(true);           // keep callback after this call
-                            receivingCallbackContext.sendPluginResult(result);
 						boolean isSsidHidden = false;
 						if (isSsidHiddenStr.equals("YES")) {
 							isSsidHidden = true;
 						}
-						int taskResultCount = Integer.parseInt(taskResultCountStr);
-						synchronized (mLock) {
 							mEsptouchTask = new EsptouchTask(apSsid, apBssid, apPassword,
 							isSsidHidden, cordova.getActivity());
 							mEsptouchTask.setEsptouchListener(myListener);
 						}
+
 						List<IEsptouchResult> resultList = mEsptouchTask.executeForResults(taskResultCount);
+						IEsptouchResult firstResult = resultList.get(0);
+						if (!firstResult.isCancelled()) {
+							int count = 0;
+							final int maxDisplayCount = taskResultCount;
+							if (firstResult.isSuc()) {
+								// StringBuilder sb = new StringBuilder();
+								// for (IEsptouchResult resultInList : resultList) {
+									// sb.append("device"+count+",bssid="
+											// + resultInList.getBssid()
+											// + ",InetAddress="
+											// + resultInList.getInetAddress()
+													// .getHostAddress() + ".");
+									// count++;
+									// if (count >= maxDisplayCount) {
+										// break;
+									// }
+								// }
+								// if (count < resultList.size()) {
+									// sb.append("\nthere's " + (resultList.size() - count)
+											// + " more resultList(s) without showing\n");
+								// }
+								PluginResult result = new PluginResult(PluginResult.Status.OK, "finished");
+								result.setKeepCallback(true);           // keep callback after this call
+								receivingCallbackContext.sendPluginResult(result);
+								//receivingCallbackContext.success("finished");
+							} else {
+								PluginResult result = new PluginResult(PluginResult.Status.ERROR, "Esptouch fail");
+								result.setKeepCallback(true);           // keep callback after this call
+								receivingCallbackContext.sendPluginResult(result);
+							}
 						}
 					}
-				}
+				}//end runnable
 			);
-						
-			PluginResult result = new PluginResult(PluginResult.Status.NO_RESULT);
-			result.setKeepCallback(true); // keep callback in order to call it later
-			receivingCallbackContext.sendPluginResult(result);
 			return true;
 		}else{
-			callbackContext.error("function name error");
+			callbackContext.error("can not find the function "+action);
 			return false;
 		}
 		
 	}
 
-	
-	//after get result
-	private void onEsptoucResultAddedPerform(final IEsptouchResult result) {
-				String text = result.getBssid() + " is connected to the wifi";
-				receivingCallbackContext.success("enter onEsptoucResultAddedPerform"+text);//-----------------
-				PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, text);
-                pluginResult.setKeepCallback(true);           // keep callback after this call
-				receivingCallbackContext.sendPluginResult(pluginResult);
-				
-	}
 	//listener to get result
 	private IEsptouchListener myListener = new IEsptouchListener() {
 
 		@Override
 		public void onEsptouchResultAdded(final IEsptouchResult result) {
-			receivingCallbackContext.success("enter onEsptouchResultAdded");//----------------
-			onEsptoucResultAddedPerform(result);
+				String text = "bssid="+ result.getBssid()+",InetAddress="+result.getInetAddress().getHostAddress();
+				PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, text);
+                pluginResult.setKeepCallback(true);           // keep callback after this call
+				receivingCallbackContext.sendPluginResult(pluginResult);
 		}
 	};
 	
