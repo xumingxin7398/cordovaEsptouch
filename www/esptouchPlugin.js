82814d8b08cdadd4ab946a30816c7d682939c4b0
xumingxin <408676083@qq.com>
2015-11-24 16:58:50 +0800

    add ios

--- www/esptouchPlugin.js
+++ www/esptouchPlugin.js
@@ -4,5 +4,9 @@ cordova = require('cordova');
 module.exports = {
 	smartConfig:function(apSsid,apBssid,apPassword,isSsidHiddenStr,taskResultCountStr,successCallback, errorCallback){
 		exec(successCallback, errorCallback, "esptouchPlugin", "smartConfig", [apSsid,apBssid,apPassword,isSsidHiddenStr,taskResultCountStr]);
+	},
+	
+	cancelConfig:function(successCallback, errorCallback){
+		exec(successCallback, errorCallback, "esptouchPlugin", "cancelConfig", []);
 	}
 }
